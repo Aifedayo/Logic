f57bf253e6c3abd35217228c0a1de0b910cd0784
Aifedayo <akeemifedayolag@gmail.com>
2021-01-06 12:00:00 +0100

    volume of a sphere

--- vol_of_sphere.py
+++ vol_of_sphere.py
@@ -3,15 +3,15 @@ A function for calculating the volume of a Sphere
 """
 
 def vol_of_sphere(rad):
-    print ((4/3)*(3.14)*(rad**3))
+    return ((4/3)*(3.14)*(rad**3))
 
 vol_of_sphere(int(input('Enter the radius of the sphere: ')))
 
 """
 OR
 """
-# from math import pi
-# def vol_of_sphere2(rad):
-#     return ((4/3)*(pi)*(rad**3))
+from math import pi
+def vol_of_sphere2(rad):
+    return ((4/3)*(pi)*(rad**3))
 
-# vol_of_sphere2(int(input('Enter the radius of the sphere: ')))
+vol_of_sphere2(int(input('Enter the radius of the sphere: ')))
