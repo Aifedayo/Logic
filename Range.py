abe04a5f53b9ffda912d5c7257709da785d516f3
Aifedayo <akeemifedayolag@gmail.com>
2021-01-07 12:00:00 +0100

    number in range

--- Range.py
+++ Range.py
@@ -2,10 +2,10 @@
 Here is a func that checks whether a number is in a given range
 """
 
-def ran_check(num, low, high):
+def range_check(num, low, high):
     if num in range(low, high+1):
         print (f'{num} is in the range of {low} and {high}')
     else:
         print(f'{num} is outside the range')
 
-ran_check(5,2,9)
+range_check(5,2,9)
