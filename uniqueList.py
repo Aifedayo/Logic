45b632e353e082ed1eb580fb6390844417a5af1a
Aifedayo <akeemifedayolag@gmail.com>
2021-01-10 12:00:00 +0100

    a list that returns a unique list

--- uniqueList.py
+++ uniqueList.py
@@ -3,11 +3,12 @@ A python list that returns a unique list
 Sample List : [1,1,1,1,2,2,3,3,3,3,4,5]
 Unique List : [1,2,3,4,5]
 """
-def unique_list(mylist):
-    mynewlist = set(mylist)
-    print(mynewlist)
 
-unique_list([1,1,1,1,2,2,2,4,4,4,5])
-"""
-Second Method
-"""
+def unique_list2(mylist):
+    newlist = []
+    for num in mylist:
+        if num not in newlist:
+            newlist.append(num)
+    print(newlist)
+
+unique_list2([1,2,2,2,3,4,4,4,5])
