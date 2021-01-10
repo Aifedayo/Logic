876e02cc772905727187cc54735833a21f77a574
Aifedayo <akeemifedayolag@gmail.com>
2021-01-10 12:00:00 +0100

    a list that returns a unique list

--- uniqueList.py
+++ uniqueList.py
@@ -1,3 +1,13 @@
 """
 A python list that returns a unique list
+Sample List : [1,1,1,1,2,2,3,3,3,3,4,5]
+Unique List : [1,2,3,4,5]
+"""
+def unique_list(mylist):
+    mynewlist = set(mylist)
+    print(mynewlist)
+
+unique_list([1,1,1,1,2,2,2,4,4,4,5])
+"""
+Second Method
 """
