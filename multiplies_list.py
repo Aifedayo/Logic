26beb1f8bd4329b3a2bf43cb96e2e78152b3c109
Aifedayo <akeemifedayolag@gmail.com>
2021-01-11 12:00:00 +0100

    multiplies a list

--- multiplies_list.py
+++ multiplies_list.py
@@ -3,10 +3,13 @@ A function to multiply all numbers in a list
 """
 
 def multiply_list(mylist):
-    total = 1
+    total_multiple = 1
+    total_sum = 0
     for num in mylist:
-        total += num
-    print(total)
+        total_multiple *= num
+        total_sum += num
+    print(f'Here is the total multiplies of the list: {total_multiple}')
+    print(f'Here is the total sum of the list: {total_sum})
 
 
 mylist = [1,2,4,5]
