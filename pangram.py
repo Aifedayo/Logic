ca875eb895f4e39722aade382c10479f6d034dc2
Aifedayo <akeemifedayolag@gmail.com>
2021-01-16 12:00:00 +0100

    pangram

--- pangram.py
+++ pangram.py
@@ -1,4 +1,11 @@
 """
 A function to check if a string is pangram or not
 """
+import string
 
+def pangram(str1):
+    alphabet = string.ascii_lowercase
+    alphaset = set(alphabet)
+    #print(alphabet)
+
+pangram(str1='Hello')
