66988375fdfaa4be3b6d132c7200924cb63d62bb
Aifedayo <akeemifedayolag@gmail.com>
2021-01-18 12:00:00 +0100

    pangram

--- pangram.py
+++ pangram.py
@@ -6,9 +6,9 @@ import string
 def pangram(str1):
     alphabet = string.ascii_lowercase
     alphaset = set(alphabet)
-    mystr1 = str1.replace(' ','').lower()
+    mystr1 = set(str1.replace(' ','').lower())
     print(mystr1)
-    if set(mystr1) == alphaset:
+    if mystr1 == alphaset:
         print(f'{str1} is a pangram')
     else:
         print(f'{str1} is not a pangram')
