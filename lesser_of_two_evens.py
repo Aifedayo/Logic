0deb680f9de2b2e09689ad8f47c3b9df80fc8a6f
Aifedayo <akeemifedayolag@gmail.com>
2021-01-21 12:00:00 +0100

    lesser of two evens docstring

--- lesser_of_two_evens.py
+++ lesser_of_two_evens.py
@@ -1,6 +1,7 @@
 """
-A functio that returns the lesser of two given
+A function that returns the lesser of two given
 numbers if both numbers are even, but greater if
 one or both numbers are old
 """
 
+def lesser_of_even(number)
