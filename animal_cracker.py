b81d12e3b1b6f5118b060f53d84966e1fae56be3
Aifedayo <akeemifedayolag@gmail.com>
2021-01-27 12:00:00 +0100

    animal cracker solution

--- animal_cracker.py
+++ animal_cracker.py
@@ -2,3 +2,9 @@
 A function that takes two-word string and returns
 True if both words begin with the same letter
 """
+
+def animal_cracker(str1, str2):
+    if str1[0] == str2[0]:
+        print(f'{str1} and {str2} both have the same beginning word')
+
+animal_cracker('lion', 'lizard')
