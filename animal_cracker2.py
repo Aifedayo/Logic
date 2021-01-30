0a693a0da3ecf508a0e054b98d4507d08fbf4827
Aifedayo <akeemifedayolag@gmail.com>
2021-01-30 12:00:00 +0100

    animal cracker2 solution

--- animal_cracker2.py
+++ animal_cracker2.py
@@ -2,3 +2,9 @@
 A function that takes two-word string and returns
 True if both words begin with the same letter
 """
+
+def animal_cracker(string):
+    mystring = string.split(' ')
+    if mystring[0][0] == mysytring[1][0]:
+
+animal_cracker('Levelhead llama')
