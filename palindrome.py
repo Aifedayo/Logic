0797f5f74a4eb4619972de45bd9dd560f3d1ff98
Aifedayo <akeemifedayolag@gmail.com>
2021-02-04 12:00:00 +0100

    position docstring

--- palindrome.py
+++ palindrome.py
@@ -1,9 +1,8 @@
-'''
-Python function that checks whether a word 
-or phrase is palindrome or not
-'''
-
 def palindrome(string):
+    '''
+    Python function that checks whether a word 
+    or phrase is palindrome or not
+    '''
     new_string = string.replace(' ','')
     if new_string == new_string[::-1]:
         print(f'{string} is a palindrome')
