e63c8124fce2c4e1a166b2116237eb9b00eabe3e
Aifedayo <akeemifedayolag@gmail.com>
2021-02-06 12:00:00 +0100

    position docstring

--- animal_cracker.py
+++ animal_cracker.py
@@ -1,9 +1,8 @@
-"""
-A function that takes two strings and returns
-True if both words begin with the same letter
-"""
-
 def animal_cracker(str1, str2):
+    """
+    A function that takes two strings and returns
+    True if both words begin with the same letter
+    """
     if str1[0] == str2[0]:
         print(f'{str1} and {str2} both have the same beginning letter')
     else:
