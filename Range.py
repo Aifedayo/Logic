61fff2542a7a90a483bec05080f5d388da87be67
Aifedayo <akeemifedayolag@gmail.com>
2021-02-08 12:00:00 +0100

    position docstring

--- Range.py
+++ Range.py
@@ -1,8 +1,8 @@
-"""
-Here is a func that checks whether a number is in a given range
-"""
-
 def range_check(num, low, high):
+    """
+    Here is a func that checks whether a number is in a given range
+    """
+
     if num in range(low, high+1):
         print (f'{num} is in the range of {low} and {high}')
     else:
