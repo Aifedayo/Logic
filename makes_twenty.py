2532d2ce34e0a10110b6a2e8da26a6209e6452ab
Aifedayo <akeemifedayolag@gmail.com>
2021-02-11 12:00:00 +0100

    makes twenty

--- makes_twenty.py
+++ makes_twenty.py
@@ -1,7 +1,13 @@
 def makes_twenty(num1, num2):
-    pass
+    """
+    A function that return True if the sum of the integers is 20
+    or if one of the integers is 20
+    """
+    if num1 + num2 == 20:
+       return True
+    elif num1 == 20 or num2 == 20:
+        return True
+    else:
+        return False
 
-"""
-A function that return True if the sum of the integers is 20
-or if one of the integers is 20
-"""
+makes_twenty(13,8)
