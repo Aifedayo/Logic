d7c899cdf89d147143d6156e98fc6ae03954accb
Aifedayo <akeemifedayolag@gmail.com>
2021-02-15 12:00:00 +0100

    macdonald solution

--- macdonald.py
+++ macdonald.py
@@ -2,3 +2,7 @@ def macdonald(str):
     """
     A function that capitalizes the first and fourth letters
     """
+    mystr = str[:3].capitalize()
+    mystr_middle = str[3:].capitalize()
+    return mystr+mystr_middle
+macdonald('zainab')
