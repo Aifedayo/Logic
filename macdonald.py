5a72ce7023d52071e5efa2e750bd4bd283e16bb4
Aifedayo <akeemifedayolag@gmail.com>
2021-02-21 12:00:00 +0100

    macdonald

--- macdonald.py
+++ macdonald.py
@@ -2,7 +2,10 @@ def macdonald(str):
     """
     A function that capitalizes the first and fourth letters
     """
-    mystr = str[:3].capitalize()
-    mystr_middle = str[3:].capitalize()
-    return mystr+mystr_middle
+    if len(str) > 3:
+        mystr = str[:3].capitalize()
+        mystr_middle = str[3:].capitalize()
+        return mystr+mystr_middle
+    else:
+        return f'String too short'
 macdonald('zainab')
