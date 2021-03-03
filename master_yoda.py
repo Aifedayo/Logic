52817cb9c25c2fecc09ed3919981553cc3f22314
Aifedayo <akeemifedayolag@gmail.com>
2021-03-03 12:00:00 +0100

    master yoda solution

--- master_yoda.py
+++ master_yoda.py
@@ -2,4 +2,7 @@ def master_yoda(string):
     """
     A function that return a sentence with the words reversed
     """
-    pass
+    new_string = string.split()[::-1]
+    print(new_string)
+
+master_yoda()
