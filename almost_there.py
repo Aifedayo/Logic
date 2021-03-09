6ce608061e843b95d7e5703d632a2f694f181e33
Aifedayo <akeemifedayolag@gmail.com>
2021-03-09 12:00:00 +0100

    almost here docstring

--- almost_there.py
+++ almost_there.py
@@ -3,4 +3,6 @@ def almost_there(num):
     A function taht return True if n is within 10 of either
     100 or 200
     """
-    pass
+    print(abs(100-num)<=10) or abs(200-num) <=10))
+
+almost_there(94)
